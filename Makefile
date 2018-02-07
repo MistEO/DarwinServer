a44abf9d73d8561391cd349dd055a7c6c3ef8b00
MrEO <mistereo@hotmail.com>
2018-02-07 15:43:24 +0800

    finished multiple-thread server

--- Makefile
+++ Makefile
@@ -2,21 +2,25 @@
 SOURCE = main.cpp
 
 #可执行文件名称
-TARGET = server
+TARGET = server.o
 
 #使用g++编译，且以C++11标准
 CXX = g++ -Wall -std=c++17
 
 #opencv需要的参数
-CFLAGS = #`pkg-config opencv --cflags --libs`
+#CFLAGS = `pkg-config opencv --cflags --libs`
+
+LIBS = -lpthread
 
 TARGET:
-	    $(CXX) -o $(TARGET) $(SOURCE) $(CFLAGS) $(UFLAGS)
+	$(CXX) -o $(TARGET) $(SOURCE) $(CFLAGS) $(LIBS)
 
 clean:
-		rm -f *.a *.o *.ymal $(TARGET) core *~ *.so *.lo *.swp
+	rm -f *.a *.o *.ymal $(TARGET) core *~ *.so *.lo *.swp
 
 # useful to make a backup "make tgz"
-tgz: clean
+tgz:
+	clean
 	mkdir -p backups
-	tar czvf ./backups_`date +"%Y_%m_%d_%H.%M.%S"`.tgz --exclude backups *
+	tar czvf ./backups_`date +"%Y_%m_%d_%H.%M.%S"`.tgz.gz --exclude backups *
+	rm -rf backups
--- main.cpp
+++ main.cpp
@@ -1,35 +1,67 @@
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <iostream>
-#include <thread>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+void * client_fun(void * arg);
 
 int main(){
     //创建套接字
-    int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
     //将套接字和IP、端口绑定
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
-    serv_addr.sin_family = AF_INET;  //使用IPv4地址
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
-    serv_addr.sin_port = htons(1240);  //端口
-    bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    struct sockaddr_in server_address;
+    bzero(&server_address, sizeof(server_address));  //初始化用NULL填充
+    server_address.sin_family = AF_INET;  //使用IPv4地址
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
+    server_address.sin_port = htons(1240);  //端口
+    bind(socket_fd, (struct sockaddr*)&server_address, sizeof(server_address));
+
     //进入监听状态，等待用户发起请求
-    listen(serv_sock, 20);
-    //接收客户端请求
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size = sizeof(clnt_addr);
-    int clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-    //向客户端发送数据
-    char str[] = "Hello World!";
-    write(clnt_sock, str, sizeof(str));
-   
-   getchar();
+    listen(socket_fd, 20);
+
+    //主循环，接受客户端请求，开启新线程接受和发送消息
+    while (true) {
+        struct sockaddr_in client_address;
+        socklen_t client_address_size = sizeof(client_address);
+        //接受客户端请求
+        int client_sock = accept(socket_fd, (struct sockaddr*)&client_address, &client_address_size);
+
+        //输出客户端Ip地址及端口号
+        char client_ip[INET_ADDRSTRLEN] = "";
+        inet_ntop(AF_INET, &client_address.sin_addr, client_ip, INET_ADDRSTRLEN);
+        printf("Accepted, ip=%s, port=%d\n", client_ip, ntohs(client_address.sin_port));
+
+        //创建并分离线程
+        pthread_t tid;
+        pthread_create(&tid, NULL, client_fun, &client_sock);
+        pthread_detach(tid);
+    }
+    
     //关闭套接字
-    close(clnt_sock);
-    close(serv_sock);
+    close(socket_fd);
     return 0;
+}
+
+void * client_fun(void * arg)
+{
+    int recv_len = 0;
+    char recv_buf[1024] = ""; 
+    int connfd = *(int*)arg;
+    
+    while((recv_len = recv(connfd, recv_buf, sizeof(recv_buf), 0)) > 0) {  
+        printf("recv_buf: %s\n", recv_buf); // 打印数据  
+        send(connfd, recv_buf, recv_len, 0); // 给客户端回数据
+        bzero(recv_buf, sizeof(recv_buf));
+    }
+
+    printf("client closed\n");
+    //关闭套接字
+    close(connfd);
+
+    return NULL;
 }
